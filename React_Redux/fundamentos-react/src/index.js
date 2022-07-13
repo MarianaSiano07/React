7e7c5b58bb2503d7facbc9ea725ab31dacd2e79a
Mariana <maryhsiano@hotmail.com>
2022-07-13 16:46:15 -0300

    Modificando arquivo js e criando um arquivo css

--- React_Redux/fundamentos-react/src/index.js
+++ React_Redux/fundamentos-react/src/index.js
@@ -1,4 +1,8 @@
+import './index.css' //Importe do CSS
 import ReactDOM from 'react-dom'
+import React from 'react'
 
 const elemento = document.getElementById('root')
-ReactDOM.render('Olá, React!!', elemento)
+const tag = <strong>Olá, React!!</strong>
+
+ReactDOM.render(<div>{tag}</div>, elemento)
